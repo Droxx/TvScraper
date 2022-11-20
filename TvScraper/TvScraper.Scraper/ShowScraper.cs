78303684ee6e0335eaa9e22833f8f57eb7f34545
Michael Stocks <michaelstocks1@gmail.com>
2022-11-20 11:32:19 +0100

    Show scraping is going well (awaiting refactoring)

--- TvScraper/TvScraper.Scraper/ShowScraper.cs
+++ TvScraper/TvScraper.Scraper/ShowScraper.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using TvScraper.Database;
 using TvScraper.Scraper.TvMazeModel;
 
 namespace TvScraper.Scraper
@@ -13,7 +15,7 @@ namespace TvScraper.Scraper
         {
             var client = new TvMazeClient();
             var shows = new List<Show>();
-            var page = 0;
+            var page = GetStartingPageNumber();
             IEnumerable<Show> result = null;
             do
             {
@@ -22,18 +24,65 @@ namespace TvScraper.Scraper
                 {
                     result = await client.Get<IEnumerable<Show>>("shows", token, parameters);
                 }
-                catch (KeyNotFoundException)
-                {
-                    break;
-                }
                 catch(HttpRequestException ex)
                 {
-
+                    if(ex.StatusCode == HttpStatusCode.TooManyRequests)
+                    {
+                        continue;
+                    }
+                    if(ex.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        break;
+                    }
                 }
                 shows.AddRange(result);
+                StoreBatch(result);
                 page++;
             } while (result.Count() > 0);
             return new ShowCollection() { Shows = shows };
+        }
+
+        private void StoreBatch(IEnumerable<Show> shows)
+        {
+            using(var database = new DataContext())
+            {
+                var collectionIds = shows.Select(s => s.Id);
+
+                var duplicateShows = database
+                    .Shows
+                    .Where(s => collectionIds.Contains(s.TvMazeId))
+                    .Select(s => s.TvMazeId);
+
+                var showsToBeInserted = new List<Database.Model.Show>();
+
+                foreach(var validShow in shows.Where(s => !duplicateShows.Contains(s.Id)))
+                {
+                    showsToBeInserted.Add(new Database.Model.Show
+                    {
+                        Name = validShow.Name,
+                        TvMazeId = validShow.Id,
+                        TvMazeUrl = validShow.Url
+                    });
+                }
+
+                database.Shows.AddRange(showsToBeInserted);
+                database.SaveChanges();
+            }
+        }
+
+        private int GetStartingPageNumber()
+        {
+            using(var database = new DataContext())
+            {
+                var mostRecentShow = database
+                    .Shows
+                    .OrderByDescending(s => s.TvMazeId)
+                    .FirstOrDefault();
+
+                var mostRecentId = mostRecentShow?.TvMazeId ?? 0;
+
+                return (int)Math.Floor(mostRecentId / 250.0);
+            }
         }
     }
 }
